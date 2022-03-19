2bc8d38f27539bfc4ab9aa8cc2e681bffeb79d9c
Miguel Tuda <mituconservata@gmail.com>
2022-03-19 14:06:55 +0100

    funciones trasladadas a .py

--- src/limpieza.py
+++ src/limpieza.py
@@ -67,3 +67,62 @@ def pastor_de_guiris(df, ciudades):
         
         guiris_esquilados.to_pickle(f'{ciudad}_guiris_esquilados.pkl')
     return
+
+def pulir(lista):
+    for reviu in lista:
+        reviu.pop()
+        reviu.pop(1)
+        for i in reviu:
+            if i == ' 16195 opiniones':
+                del reviu[reviu.index(i): (reviu.index(i)+5)]
+            elif i == ' Opinión traducida. Mostrar en idioma original':
+                del reviu[reviu.index(i)]
+    return  lista
+
+def quitar_respuestas(lista):
+    for reviu in lista:
+        if len(reviu)==6:
+            reviu.pop(-2)
+    return lista
+
+def dict_civitatis(lista):
+    dict_revius = {'fecha' : [],
+               'nombre' : [],
+               'procedencia' : [],
+               'opinion' : [],
+               'viajo_con' : []}
+    for reviu in lista:
+        if len(reviu)==2:
+            dict_revius['fecha'].append(reviu[0])
+            dict_revius['nombre'].append(reviu[1])
+            dict_revius['procedencia'].append('')
+            dict_revius['opinion'].append('')
+            dict_revius['viajo_con'].append('')
+        elif len(reviu)==3:
+            dict_revius['fecha'].append(reviu[0])
+            dict_revius['nombre'].append(reviu[1])
+            if len(re.findall(' Viajó.*', reviu[2]))>0:
+                dict_revius['procedencia'].append('')
+                dict_revius['opinion'].append('')
+                dict_revius['viajo_con'].append(reviu[2])
+            else:
+                dict_revius['procedencia'].append(reviu[2])
+                dict_revius['opinion'].append('')
+                dict_revius['viajo_con'].append('')
+        elif len(reviu)==4:
+            dict_revius['fecha'].append(reviu[0])
+            dict_revius['nombre'].append(reviu[1])
+            dict_revius['procedencia'].append(reviu[2])
+            if len(re.findall(' Viajó.*', reviu[3]))>0:
+                dict_revius['opinion'].append('')
+                dict_revius['viajo_con'].append(reviu[3])
+            else:
+                dict_revius['opinion'].append(reviu[3])
+                dict_revius['viajo_con'].append('')
+        else:
+            dict_revius['fecha'].append(reviu[0])
+            dict_revius['nombre'].append(reviu[1])
+            dict_revius['procedencia'].append(reviu[2])
+            dict_revius['opinion'].append(reviu[3])
+            dict_revius['viajo_con'].append(reviu[4])
+    return dict_revius
